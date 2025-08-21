4b5f820f83a1f690a5df6faa0936eb7d6be27e04
himengmengmeng <mengartisan@gmail.com>
2025-08-21 16:22:14 +0800

    open ai api key got updated

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -17,7 +17,9 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
 
 
 
